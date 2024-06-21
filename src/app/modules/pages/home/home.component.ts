c35ff54651a29bf238dee0175ffb187562b58496
O009920 <mfuentes@forum.cl>
2024-06-20 21:43:24 -0400

    creacion de pages

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CurrencyPipe, NgForOf} from "@angular/common";
+import {Products} from "../../../shared/models/products";
+import {DataService} from "../../../core/services/data/data.service";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    CurrencyPipe
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  products: Products[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getProducts().subscribe(x => {
+      this.products = x.outstanding;
+    });
+  }
+
+
+
 
 }
