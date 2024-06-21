293125a83dc879be70de217cbf4051719a17983d
O009920 <mfuentes@forum.cl>
2024-06-21 11:11:42 -0400

    avance carrusel

--- src/app/modules/pages/home/home.component.ts
+++ src/app/modules/pages/home/home.component.ts
@@ -17,19 +17,27 @@ import {DataService} from "../../../core/services/data/data.service";
 export class HomeComponent implements OnInit{
 
   products: Products[] = [];
+  chunkedProducts: Products[][] = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
     this.dataService.getProducts().subscribe(x => {
       this.products = x.outstanding;
+      this.chunkedProducts = this.chunk(this.products, 3);
     });
+  }
+
+  chunk(arr: any[], chunkSize: number): any[] {
+    let R = [];
+    for (let i = 0; i < arr.length; i += chunkSize)
+      R.push(arr.slice(i, i + chunkSize));
+    return R;
   }
 
   getStars(rating: number): string[] {
     return Array.from({ length: 5 }, (_, i) => i < rating ? 'fas fa-star text-warning' : 'far fa-star text-warning');
   }
-
 
 
 
