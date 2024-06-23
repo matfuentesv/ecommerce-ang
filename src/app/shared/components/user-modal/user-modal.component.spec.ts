8080e76ccd6eea99209e2a76006043dbab27d905
O009920 <mfuentes@forum.cl>
2024-06-23 15:33:12 -0400

    pruebas unitarias user modal

--- src/app/shared/components/user-modal/user-modal.component.spec.ts
+++ src/app/shared/components/user-modal/user-modal.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UserModalComponent } from './user-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('UserModalComponent', () => {
   let component: UserModalComponent;
@@ -8,10 +11,17 @@ describe('UserModalComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [UserModalComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        UserModalComponent,
+        BrowserAnimationsModule,
+        MatSnackBarModule,
+        ReactiveFormsModule
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(UserModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
