467402dc3c55e773d5419aad8579cf38836218ee
O009920 <mfuentes@forum.cl>
2024-06-23 15:30:28 -0400

    pruebas unitarias home

--- src/app/modules/pages/home/home.component.spec.ts
+++ src/app/modules/pages/home/home.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import {DataService} from "../../../core/services/data/data.service";
+
+declare let $: any;
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,10 +13,21 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
-    
+      imports: [HomeComponent, HttpClientTestingModule, RouterTestingModule],
+      providers: [DataService],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    // Mockear jQuery
+    (window as any).$ = (query: any) => {
+      return {
+        carousel: () => {},
+        on: () => {}
+      };
+    };
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -21,3 +37,4 @@ describe('HomeComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
