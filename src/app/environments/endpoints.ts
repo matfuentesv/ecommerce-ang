e5588bd9dfd9435c0bba547cedb0a7c179ee3514
O009920 <mfuentes@forum.cl>
2024-07-02 22:54:11 -0400

    se agrega enviroment

---
+++ src/app/environments/endpoints.ts
@@ -0,0 +1,12 @@
+export const endpoints = {
+  users: {
+    name: 'Users',
+    description: 'Obtiene Lista de usuarios',
+    path: 'https://firebasestorage.googleapis.com/v0/b/ecommerce-ang-8598e.appspot.com/o/users.json?alt=media'
+  },
+  products: {
+    name: 'Products',
+    description: 'Obtiene lista de productos',
+    path: 'https://firebasestorage.googleapis.com/v0/b/ecommerce-ang-8598e.appspot.com/o/data.json?alt=media&token=d23581ff-7949-4667-8f7b-07532eb98f9e'
+  }
+}
